ee8623ab6ff512e2a22ba92b9c395092e39290f3
Lyte Onyema <lyte.onyema@gmail.com>
2022-08-14 06:48:52 +0100

    added procfile for Heroku

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -36,7 +36,10 @@ class NewPasswordController extends Controller
         $request->validate([
             'token' => ['required'],
             'email' => ['required', 'email'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()->min(8)
+                ->mixedCase()
+                ->numbers()
+                ->uncompromised()],
         ]);
 
         // Here we will attempt to reset the user's password. If it is successful we
