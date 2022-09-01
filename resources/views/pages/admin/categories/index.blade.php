aa3456280da223a4f4aa747f6f6aec2f9a7b43c0
Lyte Onyema <lyte.onyema@gmail.com>
2022-09-01 19:56:15 +0100

    view for categories

--- resources/views/pages/admin/categories/index.blade.php
+++ resources/views/pages/admin/categories/index.blade.php
@@ -12,15 +12,45 @@
             <x-auth-session-status class="mb-7 text-center"/>
 
             <!-- New Category form --->
-            <div class="bg-white p-6 mt-7 overflow-hidden shadow-sm sm:rounded-lg">
+            <section>
+                <div class="px-4 py-2 text-sm text-indigo-600 font-bold">
+                    Add New Category
+                </div>
+                <div class="bg-white px-4 py-6 overflow-hidden shadow-sm sm:rounded-lg">
+                    <form action="">
+                        @csrf
+                        <div class="grid grid-cols-8">
+                            <div class="cols-span-2">
+                                <x-label for="message" class="font-semibold pb-3" :value="__('Category Name')" />
+                            </div>
+                            <div class="col-span-4">
+                                <x-input type="text" id="message" class="block w-full" placeholder="Enter the name of the new category for tasks" value="{{ old('message') }}"/>
+                                <x-form-input-error :inputName="$error = 'message'" />
+                            </div>
 
-            </div>
+                            <div class="flex items-center justify-end">
 
-            <div class="bg-white p-6 mt-7 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="pt-6 mt-4 mb-3">
-                    <div class="overflow-x-auto relative  rounded-md">
-                        <table class="min-w-max md:w-full text-sm text-left text-gray-500">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-100">
+                                <x-button class="ml-3">
+                                    {{ __('Send') }}
+                                </x-button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </section>
+
+            <section class="mt-7 ">
+                <div class="px-4 py-2 text-sm text-indigo-600 font-bold">
+                    Category list
+                </div>
+                <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="pb-7 border-b">
+                        <h3 class="text-xl font-semibold">{{ __('Showing the list of all existing categories.') }}</h3>
+                    </div>
+                    <div class="pt-6 mt-4 mb-3">
+                        <div class="overflow-x-auto relative  rounded-md">
+                            <table class="min-w-max md:w-full text-sm text-left text-gray-500">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-100">
                                 <tr>
                                     <th scope="col" class="py-3 px-6">
                                         Name
@@ -32,20 +62,20 @@
                                         Action
                                     </th>
                                 </tr>
-                            </thead>
-                            <tbody>
-                            @if($categories->count() < 1)
-                                <tr>
-                                    <td class="py-4 px-6 font-semibold text-purple-500 whitespace-nowrap text-center" colspan="2">
-                                        No Category has been created for tasks
-                                    </td>
-                                </tr>
-                            @else
-                                @foreach($categories as $category)
+                                </thead>
+                                <tbody>
+                                @if($categories->count() < 1)
                                     <tr>
-                                        <th scope="row" class="border-b border-slate-100 py-4 px-6 font-medium text-gray-900 whitespace-nowrap">{{ $category->name }}</th>
+                                        <td class="py-4 px-6 font-semibold text-purple-500 whitespace-nowrap text-center" colspan="2">
+                                            No Category has been created for tasks
+                                        </td>
+                                    </tr>
+                                @else
+                                    @foreach($categories as $category)
+                                        <tr>
+                                            <th scope="row" class="border-b border-slate-100 py-4 px-6 font-medium text-gray-900 whitespace-nowrap">{{ $category->name }}</th>
 
-                                        <td class="border-b border-slate-100 py-4 px-6">
+                                            <td class="border-b border-slate-100 py-4 px-6">
                                             <span class="bg-{{ statusColor($category->is_active) }}-100 text-{{statusColor($category->is_active)}}-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2">
                                                 {{ $category->is_active ? 'ACTIVE' : 'INACTIVE' }}
                                                 <form action="{{ route('update-status') }}" method="post" class="inline mt-1">
@@ -57,23 +87,24 @@
                                                     </button>
                                                 </form>
                                             </span>
-                                        </td>
+                                            </td>
 
-                                        <td class="border-b border-slate-100 py-4 px-6">
-                                            <form action="{{ route('update-status') }}" method="post" class="inline mt-1">
-                                                @csrf
-                                                <span onclick="event.preventDefault();
+                                            <td class="border-b border-slate-100 py-4 px-6">
+                                                <form action="{{ route('update-status') }}" method="post" class="inline mt-1">
+                                                    @csrf
+                                                    <span onclick="event.preventDefault();
                                                 this.closest('form').submit();" class="text-red-600 hover:text-red-400 hover:underline">Delete</span>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @endif
-                            </tbody>
-                        </table>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </div>
     </div>
 </x-app-layout>
