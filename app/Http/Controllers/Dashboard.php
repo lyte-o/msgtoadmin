d88f6a569d71227689945a2292291273dd75fca5
Lyte Onyema <lyte.onyema@gmail.com>
2022-09-02 01:25:11 +0100

    dashbord for tasks

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -8,9 +8,16 @@ class Dashboard extends Controller
 {
     public function index()
     {
-        $messages = auth()->user()->messages()->latest()->paginate(self::PG_NUM);
+        $tasks = auth()->user()->tasks();
+        $recents = auth()->user()->tasks()->latest()->limit(5)->get();
 
-        return view('pages.dashboard', compact('messages'));
+        $data = [];
+
+        $data['not_started'] = $tasks->where('status', 'NOT STARTED')->count();
+        $data['ongoing'] = $tasks->where('status', 'ONGOING')->count();
+        $data['completed'] = $tasks->where('status', 'COMPLETED')->count();
+
+        return view('pages.dashboard', compact('recents', 'data'));
     }
 
     public function contactAdmin()
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -7,44 +7,100 @@
 
     <div class="py-12 mx-3">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="sm:flex justify-between items-center block bg-white mb-10 px-6 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="py-6 pl-4 pr-0">
-                    <p>{{__('Hello')}} <span class="font-bold">{{ ucwords(auth()->user()->full_name) }}</span>!</p>
-                    <p class="text-slate-600 mt-1">{{__("You can now send messages to the Admin.")}}</p>
-                </div>
 
-                <div>
-                    <x-link-button href="{{ route('create-message') }}" class="m-3 p-4 px-6 sm:p-3">{{ __('New Message') }}</x-link-button>
+            <p class="px-4 py-2 text-sm text-indigo-600">{{__('Hello')}} <span class=" font-bold">{{ ucwords(auth()->user()->full_name) }}</span>!</p>
+
+            <div class="block md:grid md:grid-cols-12 gap-3 lg:gap-10 items-center">
+                <div class="md:col-span-4 bg-white mb-10 px-6 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="py-3 pl-4 pr-0 flex justify-between">
+                        <p class="text-slate-500 my-2 uppercase font-semibold items-center">{{__("Tasks not Started")}}</p>
+                        <span class="font-bold text-yellow-500 text-2xl">{{ $data['not_started'] }}</span>
+                    </div>
+                </div>
+                <div class="md:col-span-4 bg-white mb-10 px-6 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="py-3 pl-4 pr-0 flex justify-between">
+                        <p class="text-slate-500 my-2 uppercase font-semibold items-center">{{__("Ongoing Tasks")}}</p>
+                        <span class="font-bold text-indigo-600 text-2xl">{{ $data['ongoing'] }}</span>
+                    </div>
+                </div>
+                <div class="md:col-span-4 bg-white mb-10 px-6 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="py-3 pl-4 pr-0  flex justify-between items-center">
+                        <p class="text-slate-500 my-2 uppercase font-semibold">{{__("Completed Tasks")}}</p>
+                        <span class="font-bold text-green-600 text-2xl">{{ $data['completed'] }}</span>
+                    </div>
                 </div>
             </div>
 
+
             <x-auth-session-status class="text-center"/>
 
-            <div class="py-6 mt-7">
+            <section class="mt-7 ">
+                <div class="px-4 py-2 text-sm text-indigo-600 font-bold">
+                    Recent Tasks
+                </div>
                 <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="pt-3 pb-7 border-b">
-                        <h3 class="text-xl font-semibold">{{__('Showing all your messages sent to the Admin.')}}</h3>
-                    </div>
-                    <div class="pt-6 mt-4 mb-3">
-                        <div class="overflow-x-auto relative rounded-md">
+                    <div class="pt-6 mt-4">
+                        <div class="overflow-x-auto relative  rounded-md">
                             <table class="min-w-max md:w-full text-sm text-left text-gray-500">
                                 <thead class="text-xs text-gray-700 uppercase bg-gray-100">
                                 <tr>
-                                    <th scope="col" class="py-3 px-6">Date & Time</th>
-                                    <th scope="col" class="py-3 px-6">Message</th>
+                                    <th scope="col" class="py-3 px-6">
+                                        S/N
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Title
+                                    </th>
+                                    <th>
+                                        Category
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Deadline
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Status
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Action
+                                    </th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @if($messages->count() < 1)
+                                @if($recents->count() < 1)
                                     <tr>
-                                        <td class="py-4 px-6 font-semibold text-purple-500 whitespace-nowrap text-center" colspan="2">You have not sent any message to the Admin.</td>
+                                        <td class="py-4 px-6 font-semibold text-purple-500 whitespace-nowrap text-center" colspan="5">
+                                            No Task has been added yet.
+                                        </td>
                                     </tr>
                                 @else
-                                    @foreach($messages as $message)
+                                    @php
+                                        $n = 0;
+                                    @endphp
+                                    @foreach($recents as $task)
+                                        {{ $n++ }}
                                         <tr>
-                                            <th scope="row" class="py-4 px-6 border-b border-slate-100 font-medium text-gray-900 whitespace-nowrap">{{ $message->created_at->format('d-m-Y G:i') }}</th>
+                                            <th scope="row" class="border-b border-slate-100 py-4 px-6 font-medium text-gray-900 whitespace-nowrap">{{ $n }}</th>
 
-                                            <td class="border-b border-slate-100 py-4 px-6">{{ $message->body }}</td>
+                                            <td class="border-b border-slate-100 py-4 px-6">{{ $task->title }}</td>
+                                            <td class="border-b border-slate-100 py-4 px-6">
+                                                <span class="bg-indigo-500 text-white font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2">{{ $task->category->name }}</span>
+                                            </td>
+                                            <td class="border-b border-slate-100 py-4 px-6">{{ $task->deadline->format('Y-m-d H:i') }}</td>
+                                            <td class="border-b border-slate-100 py-4 px-6">
+                                                <span class="bg-{{ statusColor($task->status) }}-100 text-{{statusColor($task->status)}}-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2">
+                                                    {{ $task->status }}
+                                                </span>
+                                            </td>
+
+                                            <td class="border-b border-slate-100 py-4 px-6">
+                                                <x-link-button href="{{ route('tasks.create') }}" class="mx-2 p-1">{{ __('Edit') }}</x-link-button>
+                                                <form action="{{ route('tasks.delete', $task->slug) }}" method="post" class="inline mt-1">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <x-link-button onclick="event.preventDefault();
+                                                this.closest('form').submit();" class="btn btn-link inline-flex justify-center items-center px-4 bg-red-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none cursor-pointer focus:border-red-900 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150 mx-2 p-1" color="red">Delete</x-link-button>
+
+                                                </form>
+                                            </td>
                                         </tr>
                                     @endforeach
                                 @endif
@@ -53,7 +109,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
 
         </div>
     </div>
